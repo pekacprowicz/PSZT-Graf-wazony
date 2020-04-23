94798af4d56115bdc1715fa0c5ca326923db793e
Eijir0 <szychiewiczmateusz@gmail.com>
2020-04-24 01:50:06 +0200

    cleanup

--- experiment.py
+++ experiment.py
@@ -1,7 +1,10 @@
 import graph_utils
+import time
 
 if __name__ == "__main__":
     graph = graph_utils.Graph("graph.input")
     print(graph.edges_dict)
     print(graph.vertices_connection)
+    start_time = time.time()
     print(graph.get_shortest_path("A", "G"))
+    print("--- %s seconds ---" % (time.time() - start_time))
--- graph_utils.py
+++ graph_utils.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+# todo no path leading to end vertex
+# todo vertice names not only letters
+# todo no need to keep all terminal paths in get_shortest_path, only the shortest one is needed
+
 
 class Graph:
 
@@ -16,6 +20,15 @@ class Graph:
             self.add_update_vertex(edge[1], edge[0])
             self.edges_dict[f"{edge[0]}-{edge[1]}"] = edge[2]
 
+    #
+    def add_update_vertex(self, vertex_id, connected_vertex):
+        try:
+            self.vertices_connection[vertex_id].add(connected_vertex)
+        except KeyError:
+            self.vertices_connection[vertex_id] = set()
+            self.vertices_connection[vertex_id].add(connected_vertex)
+
+    # returns edge cost
     def get_edge_weight(self, start, end):
         try:
             weight = self.edges_dict[f"{start}-{end}"]
@@ -26,81 +39,48 @@ class Graph:
                 weight = float('inf')
         return weight
 
-    def get_path_weight(self, path):
-        weight = 0
-        if len(path) != 1:
-            for i in range(0, len(path)-2, 2):
-                start = path[i]
-                end = path[i+2]
-                weight += self.get_edge_weight(start, end)
-
-        return weight
-
-    def add_update_vertex(self, vertex_id, connected_vertex):
-        try:
-            self.vertices_connection[vertex_id].add(connected_vertex)
-        except KeyError:
-            self.vertices_connection[vertex_id] = set()
-            self.vertices_connection[vertex_id].add(connected_vertex)
-
-    def get_shortest_path(self, begin, end, method="heuristic"):
-        if method == "heuristic":
-            return self.heuristic_search(begin, end)
-
+    # finds paths that lead to end vertex
     @staticmethod
-    def check_for_terminal_path(paths, end):
-        result = False
-        for path in paths.keys():
-            if end in path:
-                result = True
-        return result
-
-    def get_terminal_paths(self, paths, end):
+    def get_terminal_paths(paths, end):
         terminal_paths = dict()
         for path in paths.keys():
             if end in path:
-                terminal_paths.update({path: self.get_path_weight(path)})
+                terminal_paths.update({path: paths[path]})
         return terminal_paths
 
-    def heuristic_search(self, begin, end, heuristic_function="first_best", **kwargs):
+    # finds shortest path based on chosen algorithm
+    def get_shortest_path(self, start, end, algorithm="brute_force"):
         paths = dict()
-        paths[begin] = 0
+        paths[start] = 0
         terminal_paths = dict()
-        extended_paths = dict()
-        while True:  # not self.check_for_terminal_path(extended_paths, end):
-            path_to_extend, minimal_cost = self.choose_next_path(paths)
-            extended_paths = self.extend_paths(path_to_extend, heuristic_function)
+        while True:
+            path_to_extend = self.choose_next_path(paths, algorithm)
+            extended_paths = self.extend_path(path_to_extend, paths[path_to_extend])
             paths.update(extended_paths)
             del paths[path_to_extend]
             terminal_paths.update(self.get_terminal_paths(extended_paths, end))
             if terminal_paths and min(terminal_paths.values()) <= min(paths.values()):
-                return min(terminal_paths, key=terminal_paths.get), min(terminal_paths.values())
+                shortest_path = min(terminal_paths, key=terminal_paths.get)
+                return shortest_path, terminal_paths[shortest_path]
 
-    def get_outgoing_edges(self, vertex):
-        edges = dict()
-        for connected_vertex in self.vertices_connection[vertex]:
-            edges[f"{connected_vertex}"] = self.get_edge_weight(vertex, connected_vertex)
-        return edges
-
+    # chooses path to be extended next based on chosen algorithm
     @staticmethod
-    def choose_next_path(paths):
-        return min(paths, key=paths.get), min(paths.values())
+    def choose_next_path(paths, algorithm):
+        if algorithm == "brute_force":  # returns oldest path
+            return next(iter(paths))
+        elif algorithm == "first_best":  # returns path with min value
+            return min(paths, key=paths.get)
+        elif algorithm == "a_star":  # uses heuristic function to evaluate next best path todo implement
+            return next(iter(paths))
 
-    def extend_paths(self, path_to_extend, heuristic_function):
+    # extends path to all not visited outgoing vertices
+    def extend_path(self, path_to_extend, path_to_extend_cost):
         extended_paths = dict()
-        existing_path_cost = self.get_path_weight(path_to_extend)
-        last_vertex = path_to_extend[-1]
+        last_vertex = path_to_extend[-1] # todo
         outgoing_vertices = self.vertices_connection[last_vertex]
-        for vertex in outgoing_vertices:
-            if vertex in path_to_extend:
+        for next_vertex in outgoing_vertices:
+            if next_vertex in path_to_extend:
                 continue
-            extended_paths[f"{path_to_extend}-{vertex}"] = existing_path_cost + self.get_heuristic_value(f"{path_to_extend}-{vertex}", heuristic_function)
+            extended_paths[f"{path_to_extend}-{next_vertex}"] \
+                = path_to_extend_cost + self.get_edge_weight(last_vertex, next_vertex)
         return extended_paths
-
-    def get_heuristic_value(self, path, heuristic_function):
-        if heuristic_function == "some":
-            pass
-        elif heuristic_function == "first_best":
-            return self.get_edge_weight(path[-1], path[-3])
-        elif heuristic_function == "brute-force":
-            return self.edges_dict
